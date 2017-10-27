f79b9fc45431770ffba9c3779812706dd10a9eeb
lcszulpo <lcszulpo@outlook.com>
2017-10-27 00:01:03 -0200

    Update binaryGap.js

--- binaryGap.js
+++ binaryGap.js
@@ -1,1 +1,18 @@
 
+function solution(N) {
+    const binary = Math.abs(N).toString(2);
+    let currentMaximum = 0;
+    let finalMaximum = 0;
+    
+    for (let i = 0; i < binary.length; i++) {
+        currentMaximum = 0;
+        
+        while (binary[i] === '0') {
+            ++currentMaximum && ++i;
+        }
+        
+        finalMaximum = Math.max(finalMaximum, currentMaximum)
+    }
+    
+    return finalMaximum
+}
